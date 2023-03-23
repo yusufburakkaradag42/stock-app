0f73e00fa65ae79823fb5f2f7d95aacbe94d2fc2
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-03-23 20:47:39 +0300

    start

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,23 +9,16 @@ import { Link, useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { Formik, Form } from "formik"
 import TextField from "@mui/material/TextField"
-import { object, string, number, date, InferType } from "yup"
+import { object, string } from "yup"
+import LoadingButton from "@mui/lab/LoadingButton"
+import useAuthCall from "../hooks/useAuthCall"
+import LoginForm, { loginScheme } from "../components/LoginForm"
 
 const Login = () => {
   const navigate = useNavigate()
-  const { currentUser, error } = useSelector((state) => state?.auth)
+  const { currentUser, error, loading } = useSelector((state) => state?.auth)
 
-  const loginScheme = object({
-    email: string().email("Lütfen valid bir email giriniz").required(),
-    password: string().required()
-    .min(8,"password en az 8 karakter olmalıdır")
-    .max(16,"password en az 20 karakter olmalıdır")
-    .matches(/\d+/,"Password bir sayı içermlidir")
-    .matches(/[a-z]+/,"Password bir küçük harf içermlidir")
-    .matches(/[A-Z]+/,"Password bir büyük harf içermlidir")
-    .matches(/[!,?{}><%&$#£+-.]+/,"Password bir özel harf içermlidir"),
-   
-  })
+  const { login } = useAuthCall()
 
   return (
     <Container maxWidth="lg">
@@ -68,43 +61,12 @@ const Login = () => {
             initialValues={{ email: "", password: "" }}
             validationSchema={loginScheme}
             onSubmit={(values, actions) => {
-              //TODO login(values)  POST istegi
-              //TODO navigate
+              login(values)
               actions.resetForm()
               actions.setSubmitting(false)
             }}
-          >
-            {({ values, handleChange, handleBlur, errors, touched }) => (
-              <Form>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                  <TextField
-                    label="Email"
-                    name="email"
-                    id="email"
-                    type="email"
-                    variant="outlined"
-                    value={values?.email || ""}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.email && Boolean(errors.email)}
-                    helperText={touched.email && errors.email}
-                  />
-                  <TextField
-                    label="Password"
-                    name="password"
-                    id="password"
-                    type="password"
-                    variant="outlined"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.password && Boolean(errors.password)}
-                    helperText={touched.password && errors.password}
-                  />
-                </Box>
-              </Form>
-            )}
-          </Formik>
+            component={(props) => <LoginForm {...props} />}
+          ></Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
