dbda431deba201be730e3a2d8cb2bb9bfa39dd7e
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-03-29 22:33:53 +0300

    products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,13 +7,8 @@ import ProductModal from "../components/modals/ProductModal"
 import useStockCall from "../hooks/useStockCall"
 
 import * as React from "react"
-import Table from "@mui/material/Table"
-import TableBody from "@mui/material/TableBody"
-import TableCell from "@mui/material/TableCell"
-import TableContainer from "@mui/material/TableContainer"
-import TableHead from "@mui/material/TableHead"
-import TableRow from "@mui/material/TableRow"
-import Paper from "@mui/material/Paper"
+import Box from "@mui/material/Box"
+import { DataGrid } from "@mui/x-data-grid"
 
 const Products = () => {
   const { getStockData } = useStockCall()
@@ -29,6 +24,73 @@ const Products = () => {
 
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "#",
+      minWidth: 40,
+      maxWidth: 70,
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "category",
+      headerName: "Category",
+      headerAlign: "center",
+      align: "center",
+      flex: 3,
+      minWidth: 150,
+    },
+    {
+      field: "brand",
+      headerName: "Brand",
+      minWidth: 150,
+      headerAlign: "center",
+      align: "center",
+      flex: 2,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      type: "number",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 150,
+      flex: 2,
+    },
+
+    {
+      field: "stock",
+      headerName: "Stock",
+      minWidth: 100,
+      headerAlign: "center",
+      align: "center",
+      flex: 0.7,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      type: "number",
+      headerAlign: "center",
+      align: "center",
+      minWidth: 50,
+      flex: 1,
+    },
+  ]
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ]
 
   useEffect(() => {
     getStockData("products")
@@ -51,37 +113,22 @@ const Products = () => {
         setInfo={setInfo}
       />
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">#</TableCell>
-              <TableCell align="right">Category</TableCell>
-              <TableCell align="right">Brand</TableCell>
-              <TableCell align="right">Name</TableCell>
-              <TableCell align="right">Stock</TableCell>
-              <TableCell align="right">Operation</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products?.map((pro, index) => (
-              <TableRow
-                key={pro.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row" align="right">
-                  {index + 1}
-                </TableCell>
-                <TableCell align="right">{pro.category}</TableCell>
-                <TableCell align="right">{pro.brand}</TableCell>
-                <TableCell align="right">{pro.name}</TableCell>
-                <TableCell align="right">{pro.stock}</TableCell>
-                <TableCell align="right"></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Box sx={{ width: "100%" }}>
+        <DataGrid
+          autoHeight
+          rows={products}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        />
+      </Box>
     </div>
   )
 }
